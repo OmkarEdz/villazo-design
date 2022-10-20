eab9ceb09aad59779d71e1b9934b258909a3f409
prasadc01 <prasadc@edreamz.in>
2022-10-20 21:57:15 +0530

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93624b4693bdade9"; }
+/******/ 		__webpack_require__.h = function() { return "a8d861d6c29f12d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/new-development.js
+++ pages/new-development.js
@@ -9,16 +9,9 @@ import Link from 'next/link'
 
 const NewDevelopment = ({
   global,
-  newdevelopmentpage,
   footerData,
   navigation,
 }) => {
-	let propertyList = newdevelopmentpage.attributes.Proprties
-    let mainpropertyList = []
-    propertyList.forEach((element) => {
-      mainpropertyList.push(element)
-      console.log(element)
-    })
     const myLoader = ({ src, width, quality }) => {
         return `${src}?w=${width}&q=${quality || 75}`
       }
@@ -30,22 +23,123 @@ const NewDevelopment = ({
 			<div className="buttons-wrap video-wrapper team-wrapper dev-wrapper">
 				<h1 className="sold-heading">NEW DEVLOPMENT</h1>
 				<div className="property-wrap">
-					{mainpropertyList.map((element) => (
-					<div key={mainpropertyList.key} className="property-box">
+					<div className="property-box">
 						<p className="img_Wrap">
-							{/* <Image
+							<Image
 								loader={myLoader}
 								src='https://res.cloudinary.com/usability-designs/image/upload/v1666097741/property_7cac483ea2.png?updated_at=2022-10-18T12:55:41.809Z'
 								className="header_image"
 								layout="fill"
 								alt="Villazzo"
-							/> */}
-							<img src={element.propertyImageURL} />
+							/>
 						</p>
-						<h4 className="pro-name">{element.heading}</h4>
-						<p className="pro-text">{element.subheading}</p>
+						<h4 className="pro-name">The Waldorf Astoria</h4>
+						<p className="pro-text">Property Location</p>
 					</div>
-					))}
+					<div className="property-box">
+						<p className="img_Wrap">
+							<Image
+								loader={myLoader}
+								src='https://res.cloudinary.com/usability-designs/image/upload/v1666097741/property_7cac483ea2.png?updated_at=2022-10-18T12:55:41.809Z'
+								className="header_image"
+								layout="fill"
+								alt="Villazzo"
+							/>
+						</p>
+						<h4 className="pro-name">aston martin</h4>
+						<p className="pro-text">Property Location</p>
+					</div>
+					<div className="property-box">
+						<p className="img_Wrap">
+							<Image
+								loader={myLoader}
+								src='https://res.cloudinary.com/usability-designs/image/upload/v1666097741/property_7cac483ea2.png?updated_at=2022-10-18T12:55:41.809Z'
+								className="header_image"
+								layout="fill"
+								alt="Villazzo"
+							/>
+						</p>
+						<h4 className="pro-name">elysee</h4>
+						<p className="pro-text">Property Location</p>
+					</div>
+					<div className="property-box">
+						<p className="img_Wrap">
+							<Image
+								loader={myLoader}
+								src='https://res.cloudinary.com/usability-designs/image/upload/v1666097741/property_7cac483ea2.png?updated_at=2022-10-18T12:55:41.809Z'
+								className="header_image"
+								layout="fill"
+								alt="Villazzo"
+							/>
+						</p>
+						<h4 className="pro-name">baccarat</h4>
+						<p className="pro-text">Property Location</p>
+					</div>
+					<div className="property-box">
+						<p className="img_Wrap">
+							<Image
+								loader={myLoader}
+								src='https://res.cloudinary.com/usability-designs/image/upload/v1666097741/property_7cac483ea2.png?updated_at=2022-10-18T12:55:41.809Z'
+								className="header_image"
+								layout="fill"
+								alt="Villazzo"
+							/>
+						</p>
+						<h4 className="pro-name">five park</h4>
+						<p className="pro-text">Property Location</p>
+					</div>
+					<div className="property-box">
+						<p className="img_Wrap">
+							<Image
+								loader={myLoader}
+								src='https://res.cloudinary.com/usability-designs/image/upload/v1666097741/property_7cac483ea2.png?updated_at=2022-10-18T12:55:41.809Z'
+								className="header_image"
+								layout="fill"
+								alt="Villazzo"
+							/>
+						</p>
+						<h4 className="pro-name">una residences</h4>
+						<p className="pro-text">Property Location</p>
+					</div>
+					<div className="property-box">
+						<p className="img_Wrap">
+							<Image
+								loader={myLoader}
+								src='https://res.cloudinary.com/usability-designs/image/upload/v1666097741/property_7cac483ea2.png?updated_at=2022-10-18T12:55:41.809Z'
+								className="header_image"
+								layout="fill"
+								alt="Villazzo"
+							/>
+						</p>
+						<h4 className="pro-name">missoni baia</h4>
+						<p className="pro-text">Property Location</p>
+					</div>
+					<div className="property-box">
+						<p className="img_Wrap">
+							<Image
+								loader={myLoader}
+								src='https://res.cloudinary.com/usability-designs/image/upload/v1666097741/property_7cac483ea2.png?updated_at=2022-10-18T12:55:41.809Z'
+								className="header_image"
+								layout="fill"
+								alt="Villazzo"
+							/>
+						</p>
+						<h4 className="pro-name">onda residences</h4>
+						<p className="pro-text">Property Location</p>
+					</div>
+					<div className="property-box">
+						<p className="img_Wrap">
+							<Image
+								loader={myLoader}
+								src='https://res.cloudinary.com/usability-designs/image/upload/v1666097741/property_7cac483ea2.png?updated_at=2022-10-18T12:55:41.809Z'
+								className="header_image"
+								layout="fill"
+								alt="Villazzo"
+							/>
+						</p>
+						<h4 className="pro-name">monaco yacht club</h4>
+						<p className="pro-text">Property Location</p>
+					</div>
 				</div>
 				<div className="video-btn contact-btn">
 					<a href="javascript:;">Contact Us</a>
@@ -65,12 +159,10 @@ export async function getStaticProps() {
   // Run API calls in parallel
   const [
     globalRes,
-	newdevelopmentpageRes,
     footerRes,
     navigationRes,
   ] = await Promise.all([
     fetchAPI("/global", { populate: "*" }),
-	fetchAPI("/newdevelopmentpage", { populate: "*" }),
     fetchAPI("/footer", { populate: "deep" }),
     fetchAPI("/header-nav", { populate: "*" }),
   ])
@@ -78,7 +170,6 @@ export async function getStaticProps() {
   return {
     props: {
       global: globalRes.data,
-	  newdevelopmentpage: newdevelopmentpageRes.data,
       footerData: footerRes.data,
       navigation: navigationRes.data,
     },
