6bab96f4affe45c209af4ca1dbf22fb52f8818da
prasadc01 <prasadc@edreamz.in>
2022-10-21 15:42:34 +0530

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ec8a3aedc983bc4"; }
+/******/ 		__webpack_require__.h = function() { return "cd18a503fbea9d08"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
