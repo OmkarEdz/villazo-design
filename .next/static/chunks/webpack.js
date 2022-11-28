fc1d59e3d646c47106a3bd742c0cd085df147fa9
OmkarEdz <omkar.t@edreamz.in>
2022-11-28 13:32:49 +0530

    make it better

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cd5a9b12ca6fffd7"; }
+/******/ 		__webpack_require__.h = function() { return "3696e0bdcc5a70ed"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
